697ee488007b22d9e0f05e6825e860594e2d65a0
Matt Callahan <matt.callahan@concordusa.com>
2016-12-06 11:00:52 -0600

    node sass compiler, nodemon server, static routing of vendors

--- server/app.js
+++ server/app.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
 var path = require ('path');
-// var sass = require('node-sass');
+var sass = require('node-sass');
+
+
 app.use(express.static('public'));
+
+
+app.use('/bootstrap', express.static(path.resolve ('./node_modules/bootstrap/dist/css')));
+
 app.listen(3000,function(){
 
   console.log('listening on 3000');
 });
+
 app.get('/', function(req,res){
   res.send(path.resolve('index.html'));
 
 });
-// sass.render({
-//   file: path.resolve('style.scss');
-// }, function(err, res) {
-//   console.log('successfully rendering sass, yo');
-//   res.send(path.resolve('main.css'));
-// });
+
