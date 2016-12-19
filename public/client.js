2364f65b091aba6282ea31f9e452f4257cdacbbd
Matt Callahan <mattcllhn@gmail.com>
2016-12-19 12:54:38 -0600

    content moving off screen

--- public/client.js
+++ public/client.js
@@ -3,16 +3,46 @@ console.log('JS');
 $(document).ready(function(){
 	console.log('JQ');
 
-if($(document).width()>= 1200){
-	$('#social-icons').insertBefore($('#footer-img-wrapper'));
-	$('#social-icon-wrapper').insertBefore($('#social-text-wrapper'));
-}//if
+	$(window).resize(function(){
+		if($(window).width()< 1200){
+			moveTheThingBack();
+		}else if($(window).width()>= 1200){
+			moveTheThing()
+		}
+	});//window.resize
 
-});//document.width conditional
+
+
+$('#hamburger').click(function(){
+	$('#content').toggleClass('content-menu-open');
+	$('#menu').toggleClass('menu-menu-open');
+
+
+});//ham on click
+
+if($(window).width()>= 1200){
+			moveTheThing()
+		}//window.width conditional
+});//docready
 
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('testController',['$scope',function($scope){
 	console.log('NG');
+
+
 	
 }]);
+
+
+
+
+	function moveTheThing(){
+			$('#social-icons').insertBefore($('#footer-img-wrapper'));
+			$('#social-icon-wrapper').insertBefore($('#social-text-wrapper'));
+	}//moveTheThing
+	function moveTheThingBack(){
+
+			$('#social-icons').insertBefore($('#nav-li'));
+			$('#social-text-wrapper').insertBefore($('#social-icon-wrapper'));
+	}//moveTheThingBack
